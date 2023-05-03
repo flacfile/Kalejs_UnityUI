a05670e41b56ed30c8bb166c069fb278b31e4fd5
Jegors Kalejs <jegors.kalejs@gmail.com>
2023-05-03 11:00:52 +0300

    Japarveido characteru par UI elementu

---
+++ ClothesChange/Assets/Skripti/MainitLielumu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainitLielumu : MonoBehaviour {
+    public GameObject mainigaisAttels;
+    public GameObject izmSlaideris;
+    public void mainitLielumu()
+    {
+        float pasreizejaVertiba =
+            izmSlaideris.GetComponent<Slider>().value;
+        mainigaisAttels.transform.localScale = new Vector2(1f * pasreizejaVertiba, 1f * pasreizejaVertiba);
+    }
+}
