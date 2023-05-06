615d11112e4f77060cddf261ad015684d54a0f80
Jegors Kalejs <jegors.kalejs@gmail.com>
2023-05-06 14:50:18 +0300

    Japabeidz sliderus un vel vienu lietotaju

---
+++ Kalejs_UnityUI/ClothesChange/Assets/Skripti/MainitLielumu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainitLielumu : MonoBehaviour
+{
+    public GameObject mainigaisAttels;
+    public Slider platumaSlaideris;
+    public Slider garumaSlaideris;
+
+    private float platums;
+    private float garums;
+
+    private void Start()
+    {
+        platums = mainigaisAttels.transform.localScale.x;
+        garums = mainigaisAttels.transform.localScale.y;
+    }
+
+    public void mainitPlatumu()
+    {
+        float pasreizejaVertiba = platumaSlaideris.value;
+
+        mainigaisAttels.transform.localScale = new Vector2(platums * pasreizejaVertiba, garums);
+    }
+
+    public void mainitGarumu()
+    {
+        float pasreizejaVertiba = garumaSlaideris.value;
+
+        mainigaisAttels.transform.localScale = new Vector2(platums, garums * pasreizejaVertiba);
+    }
+}
---
+++ Kalejs_UnityUI/ClothesChange/Assets/Skripti/VarduParadisana.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VarduParadisana : MonoBehaviour {
+
+    public string teksts;
+    public GameObject ievadLauks;
+    public GameObject tekstaAttelosana;
+    int indekss;
+    public int vecums;
+    public void UzglabatTekstu()
+    {
+     
+        teksts = ievadLauks.GetComponent<InputField>().text;
+        
+        tekstaAttelosana.GetComponent<Text>().text =  teksts.ToUpper() + "!";
+
+    }
+
+}
